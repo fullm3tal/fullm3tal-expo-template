ef64c7c1616e913a99bf2b00ab0e43f1dfc9ea09
bheem.negi <bheem.negi3@gmail.com>
2026-06-28 18:26:27 +0530

    task: add nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
     </View>
   );
 }
