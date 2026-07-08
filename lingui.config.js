44abe9f8447b35738c3fba82ebdca52458c1ff4e
bheem.negi <bheem.negi3@gmail.com>
2026-07-09 05:05:49 +0530

    tsak: add ling ui

---
+++ lingui.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "@lingui/cli";
+
+export default defineConfig({
+  sourceLocale: "en",
+  locales: ["cs", "en"],
+  catalogs: [
+    {
+      path: "<rootDir>/src/locales/{locale}/messages",
+      include: ["src"],
+    },
+  ],
+  format: "po",
+});
