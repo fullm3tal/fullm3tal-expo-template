2c057df41c2c42ad11ea70b8ce98a6e33d5c112b
bheem.negi <bheem.negi3@gmail.com>
2026-08-08 23:55:44 +0530

    task: remove unused type

--- src/theme/types/state.ts
+++ src/theme/types/state.ts
@@ -1,7 +1,5 @@
 import type {ColorValue} from 'react-native'
 
-export type StateOpacityKey = keyof typeof tokens.md.sys.state.opacity
-
 export type StateLayer = {
   color: ColorValue
   opacity: number
